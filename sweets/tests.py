53fc65cceb07d0827ce796b7fb5abe18b936a495
Jaskier <zooom.83@mail.ru>
2021-06-08 16:22:31 +0300

    add some tests

--- sweets/tests.py
+++ sweets/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Cake, Category
 
-# Create your tests here.
+
+class CreateSweetsTestCase(TestCase):
+    def setUp(self):
+        self.test_category = Category.objects.create(title='TestCategoryTitle')
+        self.test_cake = Cake.objects.create(name='TestCakeName', description='TestCakeDesc', text='TestCakeText', category=self.test_category)
+
+    def test_create_category(self):
+        self.assertEqual(self.test_category.title, 'TestCategoryTitle')
+        self.assertEqual(self.test_category.cake_set.count(), 1)
+
+    def test_create_cake(self):
+        self.assertEqual(self.test_cake.name, 'TestCakeName')
+        self.assertEqual(self.test_cake.category.title, 'TestCategoryTitle')
