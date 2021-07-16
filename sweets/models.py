77c4f6f50bd4ab769a733bb404cb555cb2f7a641
Jaskier <zooom.83@mail.ru>
2021-07-16 14:27:07 +0300

    add slugify

--- sweets/models.py
+++ sweets/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.template.defaultfilters import slugify
 
 
 class Cake(models.Model):
@@ -9,6 +10,11 @@ class Cake(models.Model):
     image = models.ImageField(upload_to='photos/%y/%m/%d/', verbose_name='Фото', null=True)
     category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL, verbose_name='Категория')
     slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('cake_detail', kwargs={'slug': self.slug})
@@ -25,6 +31,11 @@ class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Категория')
     image = models.ImageField(upload_to='category_pic/%y/%m/%d/', blank=True, verbose_name='Фото категории')
     slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('cake_by_category', kwargs={'slug': self.slug})
--- sweets/urls.py
+++ sweets/urls.py
@@ -3,6 +3,6 @@ from .views import HomePage, CakeByCategory, CakeDetail
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
-    path('sweets/<str:slug>/', CakeByCategory.as_view(), name='cake_by_category'),
-    path('cake/<str:slug>/', CakeDetail.as_view(), name='cake_detail'),
+    path('sweets/<slug:slug>/', CakeByCategory.as_view(), name='cake_by_category'),
+    path('cake/<slug:slug>/', CakeDetail.as_view(), name='cake_detail'),
 ]
